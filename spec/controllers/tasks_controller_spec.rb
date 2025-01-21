8caf5de85c432c42091cf66f80f5049520451a24
ymmtd0x0b <ymmtd0x0b@gmail.com>
2025-01-21 13:02:23 +0900

    beforeブロックを使用したセットアップをletに置き換える(tasks_controller_spec)

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-  before do
-    @user = FactoryBot.create(:user)
-    @project = FactoryBot.create(:project, owner: @user)
-    @task = @project.tasks.create!(name: 'Test task')
-  end
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
+  let(:task) { project.tasks.create!(name: 'Test task') }
 
   describe '#show' do
     it 'JSON形式でレスポンスを返すこと' do
-      sign_in @user
-      get :show, format: :json, params: { project_id: @project.id, id: @task.id }
+      sign_in user
+      get :show, format: :json, params: { project_id: project.id, id: task.id }
       expect(response.content_type).to include 'application/json'
     end
   end
@@ -18,25 +16,25 @@ RSpec.describe TasksController, type: :controller do
   describe '#create' do
     it 'JSON形式でレスポンスを返すこと' do
       new_task = { name: 'New test task' }
-      sign_in @user
-      post :create, format: :json, params: { project_id: @project.id, task: new_task }
+      sign_in user
+      post :create, format: :json, params: { project_id: project.id, task: new_task }
       expect(response.content_type).to include 'application/json'
     end
 
     it '新しいタスクをプロジェクトに追加すること' do
       new_task = { name: 'New test task' }
-      sign_in @user
+      sign_in user
       expect do
-        post :create, format: :json, params: { project_id: @project.id, task: new_task }
-      end.to change(@project.tasks, :count).by(1)
+        post :create, format: :json, params: { project_id: project.id, task: new_task }
+      end.to change(project.tasks, :count).by(1)
     end
 
     it '認証を要求すること' do
       new_task = { name: 'New test task' }
       # ここでは敢えてログインしない
       expect do
-        post :create, format: :json, params: { project_id: @project.id, task: new_task }
-      end.to_not change(@project.tasks, :count)
+        post :create, format: :json, params: { project_id: project.id, task: new_task }
+      end.to_not change(project.tasks, :count)
       expect(response).to_not be_successful
     end
   end
